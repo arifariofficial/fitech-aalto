dde863aedd3bb4f47ef7b073eac9736cf1ca323f
Ariful Islam <ariful.islam@tietoevry.com>
2025-12-15 13:27:55 +0200

    Add object and JSON handling examples to javascript-primer.js

--- javascript-primer.js
+++ javascript-primer.js
@@ -5,3 +5,29 @@ let name = "B. Eich";
 name = "Breandan Eich";
 
 console.log(`${name} was born in ${yearOfBirth}`);
+
+// Objects
+
+const person = {
+	name: "Brendan Eich",
+	"year-of-birth": 1961,
+};
+const personNameKey = "name";
+
+console.log(person[personNameKey]);
+console.log(person["year-of-birth"]);
+
+console.log("=".repeat(60));
+
+// JSON
+const jsonString = '{"name": "Jane Doe", "yearOfbirth": 1990}';
+console.log(`\nType of jsonString: ${typeof jsonString} `);
+
+const obj = JSON.parse(jsonString);
+console.log(`\nType of obj: ${typeof obj}`);
+console.log(obj);
+
+const string = JSON.stringify(obj);
+console.log("\nStringified: ", string);
+
+console.log("=".repeat(60));
